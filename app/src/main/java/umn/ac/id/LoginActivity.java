77182e7a94a376dc14bb23c44f90d0433970a4b0
kinur28 <64748020+kinur28@users.noreply.github.com>
2021-04-26 02:34:36 +0700

    Update LoginActivity.java
    login sesuai database

--- app/src/main/java/umn/ac/id/LoginActivity.java
+++ app/src/main/java/umn/ac/id/LoginActivity.java
@@ -2,11 +2,19 @@ package umn.ac.id;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText username, password;
+    Button btnLogin;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +23,31 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        username = (EditText) findViewById(R.id.enter_username);
+        password = (EditText) findViewById(R.id.enter_pw);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        DB = new DBHelper(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(LoginActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(LoginActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Username or password is wrong", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
